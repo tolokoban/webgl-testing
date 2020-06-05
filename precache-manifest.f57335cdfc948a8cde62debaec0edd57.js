5d590c4260e1c140de5b0086e8a8f3e8fde73ef5
Tolokoban <contact@tolokoban.org>
2020-06-06 00:19:08 +0200

    Updates

---
+++ precache-manifest.f57335cdfc948a8cde62debaec0edd57.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c7f82991142860f6395042da72293a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1aaa23ea2336601c129a",
+    "url": "./static/css/2.77a929df.chunk.css"
+  },
+  {
+    "revision": "8cc0bbd05841e7f7cedd",
+    "url": "./static/css/3.663ed3ea.chunk.css"
+  },
+  {
+    "revision": "dd5d4571a16afdd44618",
+    "url": "./static/css/4.1b5b2257.chunk.css"
+  },
+  {
+    "revision": "9f61ad181397964d1c5f",
+    "url": "./static/css/5.1b5b2257.chunk.css"
+  },
+  {
+    "revision": "e92af9e3fac513e48b35",
+    "url": "./static/css/6.9b9ae968.chunk.css"
+  },
+  {
+    "revision": "af3adbf7d50d31dcc484",
+    "url": "./static/css/main.65f55929.chunk.css"
+  },
+  {
+    "revision": "1aaa23ea2336601c129a",
+    "url": "./static/js/2.90a9496f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.90a9496f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc0bbd05841e7f7cedd",
+    "url": "./static/js/3.233276fc.chunk.js"
+  },
+  {
+    "revision": "dd5d4571a16afdd44618",
+    "url": "./static/js/4.1be2ae57.chunk.js"
+  },
+  {
+    "revision": "9f61ad181397964d1c5f",
+    "url": "./static/js/5.291c80af.chunk.js"
+  },
+  {
+    "revision": "e92af9e3fac513e48b35",
+    "url": "./static/js/6.e1a70d06.chunk.js"
+  },
+  {
+    "revision": "af3adbf7d50d31dcc484",
+    "url": "./static/js/main.2526d41d.chunk.js"
+  },
+  {
+    "revision": "a5bf3dad30315ac08d1b",
+    "url": "./static/js/runtime-main.dfe2f1b4.js"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "./static/media/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "./static/media/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "./static/media/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "99be908bf9c2d4217ea763e64f5694d6",
+    "url": "./static/media/dialog.99be908b.yaml"
+  },
+  {
+    "revision": "d9c85352c2c1b6d28bbc80b1bede207b",
+    "url": "./static/media/input-date.d9c85352.yaml"
+  },
+  {
+    "revision": "ed7190eadf3846a05e7238597dfe0c3b",
+    "url": "./static/media/input-image.ed7190ea.yaml"
+  },
+  {
+    "revision": "7270440b8ddb42ebfe9fba9297a3563d",
+    "url": "./static/media/josefin.7270440b.woff2"
+  },
+  {
+    "revision": "a27a283f4f3797aa95a379521647c80a",
+    "url": "./static/media/main.a27a283f.frag"
+  },
+  {
+    "revision": "a91ec25ca7b026b0762643634d986001",
+    "url": "./static/media/main.a91ec25c.vert"
+  },
+  {
+    "revision": "cb7cba228021e2a896e097dbf3309be1",
+    "url": "./static/media/main.cb7cba22.frag"
+  },
+  {
+    "revision": "d0ebbab811d5dee2788e736fcc3ec10c",
+    "url": "./static/media/main.d0ebbab8.vert"
+  },
+  {
+    "revision": "04c7e99b2918bcab6bebc7a5c44459c5",
+    "url": "./static/media/mystery-quest-400.04c7e99b.woff2"
+  },
+  {
+    "revision": "34658b6ea7c0b599f039cc1a070af585",
+    "url": "./static/media/perspective.world-point-to-screen.34658b6e.vert"
+  },
+  {
+    "revision": "79a1763da49e723c51ba382b8165dd23",
+    "url": "./static/media/perspective.world-vector-to-screen.79a1763d.vert"
+  },
+  {
+    "revision": "d73f403cb7dedbcc76e42e5d81cb4e9b",
+    "url": "./static/media/wheel.d73f403c.png"
+  }
+]);
