dd80f8ed34794ab39211cdf3ca73039f35ae84d0
Tolokoban <contact@tolokoban.org>
2020-06-08 23:35:22 +0200

    Jump on moving plateforms.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -21,6 +21,10 @@ interface IAppState {
     scaleY: number
 }
 
+const ISLAND_A_SHIFT_X = -11
+const ISLAND_B_SHIFT_X = 11
+const LEAP_RADIUS = 6
+
 export default class App extends React.Component<IAppProps, IAppState> {
     state = {
         thickness: 2,
@@ -40,29 +44,36 @@ export default class App extends React.Component<IAppProps, IAppState> {
 
         //const car = await WorldObj.createAsync(scene, "./assets/mesh/Car.json")
         const balzac = await WorldObjFactory.createAsync(scene, "./assets/mesh/Balzac.json")
-        balzac.transfo.x = 13.5
+        balzac.transfo.x = 0
+        const hydre = await WorldObjFactory.createAsync(scene, "./assets/mesh/Hydre.json")
+        hydre.transfo.x = -LEAP_RADIUS * 0.0
+        hydre.transfo.y = 0
+        hydre.transfo.z = 1
         const islandA = await WorldObjFactory.createAsync(scene, "./assets/mesh/Island-A.json")
+        islandA.transfo.x = ISLAND_A_SHIFT_X - LEAP_RADIUS
         const islandB = await WorldObjFactory.createAsync(scene, "./assets/mesh/Island-B.json")
-        islandB.transfo.x = 30
+        islandB.transfo.x = ISLAND_B_SHIFT_X + LEAP_RADIUS
         const helixIslandB = await WorldObjFactory.createAsync(scene, "./assets/mesh/Helix.json")
-        helixIslandB.transfo.x = 30
+        helixIslandB.transfo.x = islandB.transfo.x
         helixIslandB.transfo.y = 10
         helixIslandB.transfo.sx = 10
         helixIslandB.transfo.sy = 10
         helixIslandB.transfo.sz = 10
         const stone = await WorldObjFactory.createAsync(scene, "./assets/mesh/Stone.json")
-        stone.transfo.x = 13.5
+        stone.transfo.x = 0
         const helixStone = await WorldObjFactory.createAsync(scene, "./assets/mesh/Helix.json")
-        helixStone.transfo.x = 13.5
+        helixStone.transfo.x = stone.transfo.x
         helixStone.transfo.y = -1
         helixStone.transfo.sx = 3
         helixStone.transfo.sy = 3
         helixStone.transfo.sz = 3
 
+        removeSplash()
+
         let dis = 200
         let disTarget = 35
         let lat = Calc.deg2rad(30)
-        let lng = Calc.deg2rad(-40)
+        let lng = Calc.deg2rad(140)
         let lastTime = 0
         let lat0 = lat
         let lng0 = lng
@@ -85,12 +96,6 @@ export default class App extends React.Component<IAppProps, IAppState> {
             gl.enable(gl.DEPTH_TEST)
             gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-            balzac.thickness = this.state.thickness
-            // island.color[0] = 1 - Math.abs(Math.cos(time * 0.003))
-            // island.color[1] = island.color[0] * 0.5
-            // car.y = Math.abs(Math.cos(time * 0.002)) * 3
-            // car.x = 3 + Math.sin(time * 0.002) * 3
-            // car.scaleY = this.state.scaleY
             helixIslandB.transfo.lng = -time * 0.1
             helixIslandB.transfo.y = islandB.transfo.y + 10
             helixStone.transfo.lng = -time * 0.17
@@ -107,7 +112,15 @@ export default class App extends React.Component<IAppProps, IAppState> {
             stone.transfo.y = 0.3 * (Math.cos(time * 0.001045) * Math.sin(time * 0.002742))
 
             balzac.transfo.y = 0
+            hydre.transfo.x = LEAP_RADIUS * Math.cos(time * 0.0001)
+            hydre.transfo.y = 0
+            hydre.transfo.lat = time * 0.180
+            hydre.transfo.lng = 90
+            jump(balzac, 5, time)
+            jump(hydre, 2, time)
             snapToGround(balzac, islandA, stone, islandB)
+            snapToGround(hydre, islandA, stone, islandB)
+            hydre.transfo.y += 0.9
 
             if (scene.pointer.down) {
                 lat = lat0 + scene.pointer.y - y0
@@ -130,6 +143,7 @@ export default class App extends React.Component<IAppProps, IAppState> {
             stone.paint(time)
             helixStone.paint(time)
             balzac.paint(time)
+            hydre.paint(time)
         }
 
         this.scene = scene
@@ -181,9 +195,51 @@ function snapToGround(
     stone: WorldObj,
     islandB: WorldObj
 ) {
+    let influenceA = 0
+    let influenceS = 0
+    let influenceB = 0
     const yA = islandA.transfo.y
     const yS = stone.transfo.y
     const yB = islandB.transfo.y
+    const x = Calc.clamp(character.transfo.x, -LEAP_RADIUS, LEAP_RADIUS)
+    if (x < 0) {
+        influenceS = (LEAP_RADIUS + x) / LEAP_RADIUS
+        influenceA = 1 - influenceS
+    } else {
+        influenceB = x / LEAP_RADIUS
+        influenceS = 1 - influenceB
+    }
+    character.transfo.y += yA * influenceA + yS * influenceS + yB * influenceB
+}
 
-    character.transfo.y += yS
+
+function removeSplash() {
+    const splash = document.getElementById("splash")
+    if (!splash) return
+    splash.classList.add("hide")
+    window.setTimeout(
+        () => {
+            const parent = splash.parentNode
+            if (!parent) return
+            parent.removeChild(splash)
+        },
+        1000
+    )
+}
+
+
+function jump(char: WorldObj, duration: number, time: number) {
+    const delay = 1000 * duration
+    const alpha = (time % (2 * delay)) / delay
+    if (alpha < 1) {
+        char.transfo.x = alpha * 2 * LEAP_RADIUS - LEAP_RADIUS
+        char.transfo.y = duration * Math.abs(Math.sin(alpha * 2 * Math.PI))
+        char.transfo.lng = 90
+    } else {
+        const beta = 2 - alpha
+        char.transfo.x = beta * 2 * LEAP_RADIUS - LEAP_RADIUS
+        char.transfo.y = duration * Math.abs(Math.sin(beta * 2 * Math.PI))
+        char.transfo.lng = -90
+    }
+
 }
